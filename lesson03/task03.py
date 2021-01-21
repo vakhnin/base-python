c439cbbd0946f1da607e37de9298d94221b47d1b
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-01-21 14:11:44 +0300

    Урок 3 задача 3

---
+++ lesson03/task03.py
@@ -0,0 +1,22 @@
+"""
+Реализовать функцию my_func(),
+которая принимает три позиционных аргумента,
+и возвращает сумму наибольших двух аргументов.
+"""
+
+
+def my_func(x, y, z):
+    sorted_vals = sorted((x, y, z))
+    return sorted_vals[-1] + sorted_vals[-2]
+
+
+def main():
+    x = int(input('Введите число x (целое число): '))
+    y = int(input('Введите число y (целое число): '))
+    z = int(input('Введите число z (целое число): '))
+
+    print(my_func(x, y, z))
+
+
+if __name__ == '__main__':
+    main()
