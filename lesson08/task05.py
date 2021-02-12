3eaa61be61d88f081bc134672cc4f64fd82696fb
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-02-12 15:14:18 +0300

    Урок 8 задача 5

--- lesson08/task05.py
+++ lesson08/task05.py
@@ -49,33 +49,61 @@ class Xerox(Equipment):
 
 class Storage:
     storage: {}
+    subdivisions = {}
 
-    def __init__(self):
+    def __init__(self, subdivisions: tuple):
         self.storage = {}
+        for el in subdivisions:
+            self.subdivisions[el] = {}
 
     def __repr__(self):
-        return str(self.storage)
+        return str(self.storage) + '\n' + str(self.subdivisions)
 
     def add_position(self, equipment):
         self.storage[equipment] = 0
 
+    def add_to_storage(self, item, count):
+        self.storage[item] += count
+
+    def move_to_subdivision(self, item, count, subdivision):
+        if self.storage[item] - count < 0:
+            print(f'Ошибка: Количество {self.storage[item]} '
+                  f'"{item.name}" '
+                  f'на складе меньше перемещаемого {count}')
+            return
+        self.storage[item] -= count
+        if item in self.subdivisions[subdivision].keys():
+            self.subdivisions[subdivision][item] += count
+        else:
+            self.subdivisions[subdivision][item] = count
+
 
 def main():
-    storage = Storage()
-    print(storage)
-    print()
+    storage = Storage(('отдел1', 'отдел2'))
 
-    storage.add_position(Xerox('xerox1', 15000))
-    print(storage)
-    print()
+    xerox1 = Xerox('xerox1', 15000)
+    storage.add_position(xerox1)
 
-    storage.add_position(Scaner('scaner2', 5000, is_hand_scaner=False))
-    print(storage)
-    print()
+    scaner2 = Scaner('scaner2', 5000, is_hand_scaner=False)
+    storage.add_position(scaner2)
 
-    storage.add_position(Printer('printer3', 7000))
+    printer3 = Printer('printer3', 7000)
+    storage.add_position(printer3)
     print(storage)
-    print()
+
+    print('\nДобавляем на склад:')
+    storage.add_to_storage(xerox1, 3)
+    storage.add_to_storage(scaner2, 1)
+    print(storage)
+
+    print('\nПереводим в отдел:')
+    storage.move_to_subdivision(xerox1, 2, 'отдел1')
+    print(storage)
+    storage.move_to_subdivision(xerox1, 1, 'отдел1')
+    print(storage)
+
+    print('\nПытаемся перевести больше имеющегося на складе:')
+    storage.move_to_subdivision(scaner2, 2, 'отдел2')
 
 
 if __name__ == '__main__':
