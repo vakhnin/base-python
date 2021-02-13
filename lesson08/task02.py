6add68ebb00d106f8e15afc989202a1a25be02ee
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-02-13 11:22:54 +0300

    Урок 8 задача 2. Рефакторинг

--- lesson08/task02.py
+++ lesson08/task02.py
@@ -14,21 +14,25 @@ class MyZeroDivisionError(Exception):
 
 def main():
     while True:
-        x = input('Введите делимое или "q", для выхода: ')
-        if x == 'q':
-            break
-        x = int(x)
-        y = input('Введите делитель или "q", для выхода: ')
-        if y == 'q':
-            break
-        y = int(y)
         try:
+            print('\nЕще раз')
+            x = input('Введите делимое или "q", для выхода: ')
+            if x == 'q':
+                break
+            x = int(x)
+            y = input('Введите делитель или "q", для выхода: ')
+            if y == 'q':
+                break
+            y = int(y)
+
             if y == 0:
                 raise MyZeroDivisionError('Ошибка: на ноль делить нельзя!')
             print(f'{x}/{y}={x / y}')
+        except ValueError:
+            print('Ошибка: необходимо ввести число!')
+            continue
         except MyZeroDivisionError as e:
             print(e.txt)
-        print('\nЕще раз')
 
 
 if __name__ == '__main__':
